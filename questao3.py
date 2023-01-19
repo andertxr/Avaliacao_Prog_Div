27f69cbc1f5700d1dc8270c99387e1a92aa5c646
Anderson Teixeira <65094722+andertxr@users.noreply.github.com>
2023-01-19 15:07:18 -0300

    Questao 3 da prova DIv-Magalu

---
+++ questao3.py
@@ -0,0 +1,5 @@
+# 3. Na programação é comum relacionar dados usando um campo que identifica
+# registros (id). Elabore uma modelagem de dados em que os registros estão
+# relacionados através de um campo identificador (o id), atendendo as seguintes
+# afirmações:
+
