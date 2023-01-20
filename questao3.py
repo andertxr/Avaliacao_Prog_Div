0829b249d1745b84c64d1638e2f8fd921bbd46fa
Anderson Teixeira <65094722+andertxr@users.noreply.github.com>
2023-01-20 16:17:23 -0300

    Questao 3 da prova DIv-Magalu

---
+++ questao3.py
@@ -0,0 +1,11 @@
+# 3. Na programação é comum relacionar dados usando um campo que identifica
+# registros (id). Elabore uma modelagem de dados em que os registros estão
+# relacionados através de um campo identificador (o id), atendendo as seguintes
+# afirmações:
+
+estados = [{'id': 1, 'estado': 'Goiás'}, {'id': 2, 'estado': 'Paraná'}]
+
+cidades = [{'id': 11, 'cidade': 'Diamantina', 'estado_id': 1},
+           {'id': 12, 'cidade': 'Noronha', 'estado_id': 2}]
+bairros = [{'id': 21, 'bairro': 'Betânia', 'cidade_id': 11, 'estado_id': 1}, {'id': 22, 'bairro': 'Agostinho',
+ 'cidade_id': 12, 'estado_id': 2}, {'id': 23, 'bairro': 'Natal', 'cidade_id': 12, 'estado_id': 2}]
